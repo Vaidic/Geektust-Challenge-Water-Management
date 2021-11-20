bb78c0c30340f67ec85c43af0e7f688c007d89d5
Vaidic Joshi <vaidic@vaidicjoshi.in>
2021-11-20 13:42:26 +0530

    Update gradle properties

--- src/main/java/in/vaidicjoshi/geektrust/backend/watermanagement/WaterBillApp.java
+++ src/main/java/in/vaidicjoshi/geektrust/backend/watermanagement/WaterBillApp.java
@@ -5,4 +5,15 @@ package in.vaidicjoshi.geektrust.backend.watermanagement;
  * @date 20/11/21
  */
 public class WaterBillApp {
+
+  public static void main(String[] args) {
+      // Try to read input file and generate bill
+//      try{
+//          if(args.length!=1){
+//              throw new WaterBillException("Input file not supplied. Please provide the input file");
+//          }
+//          String filePath = args[0];
+//          // parse the onput file
+//      }
+  }
 }
